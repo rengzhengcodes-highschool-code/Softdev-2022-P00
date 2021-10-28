e1fb66545cd0cc81b2e8c9b7970ca31589cd6ae7
rengzhengcodes <rzhengcontacts@gmail.com>
2021-10-28 15:09:57 -0400

    necessary boiler for create_story made and a delete function that closes the database also made

--- app/story_manager.py
+++ app/story_manager.py
@@ -2,10 +2,27 @@ import sqlite3
 
 class Story_manager:
 	def __init__(self, db_file:str = "stories.db"):
-		```sets up the database that stores all the stories```
+		'''sets up the database that stores all the stories'''
 		self.DB_FILE = db_file
 		self.db = sqlite3.connect(self.DB_FILE)
 		self.c = self.db.cursor()
+		# creates stories table if it does not exist
+		self.c.execute("CREATE TABLE IF NOT EXISTS stories(story PRIMARY KEY)")
 
-	def create_story(self, title:str):
-			
+	def create_story(self, story:str) -> bool:
+		'''Creates a story and returns if successful'''
+		# adds a story to the list
+		try: # if an error happens, it will catch it and say something is wrong with the story creation
+			self.c.execute("INSERT INTO stories (story) VALUES(?)", (story,))
+			return True
+		except: # if it does not it won't
+			return False
+		# TODO: We should make an exception for overlapping story titles and invalid titles
+
+	def __del__(self):
+		self.db.commit()
+		self.db.close()
+
+if __name__ == "__main__":
+	sm = Story_manager()
+	sm.create_story("test")
