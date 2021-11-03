e67066605ea8cb121ca21038247246408537efed
rengzhengcodes <rzhengcontacts@gmail.com>
2021-11-03 14:24:44 -0400

    added insertion exceptions

--- app/story_manager.py
+++ app/story_manager.py
@@ -35,7 +35,10 @@ class Story_manager:
 
 	def get_last_entry(self, story:str) -> str:
 		'''Gets the latest entry in a story'''
-		self.c.execute("SELECT * FROM contributions WHERE story=? ORDER BY ordinal", (story,))
+		if story in self.get_catalog():
+			self.c.execute("SELECT * FROM contributions WHERE story=? ORDER BY ordinal", (story,))
+		else:
+			raise InputError("Story does not exist")
 		entries = self.c.fetchall()
 		return entries[-1] #return last entry
 
@@ -53,7 +56,9 @@ class Story_manager:
 	def insert_entry(self, usr:str, story:str, addition:str) -> bool:
 		'''Inserts an entry into the story. Notes user.'''
 		last_ordinal = self.get_last_entry(story)[-1] #the number of the last entry
-		if usr not in self.get_story_contributors(story):
+		if story not in sm.get_catalog():
+			raise InputError("Story does not exist")
+		elif usr not in self.get_story_contributors(story):
 			self.c.execute("INSERT INTO contributions(contributor, story, addition, ordinal) VALUES(?,?,?,?)", (usr, story, addition, last_ordinal + 1)) #inserts the contribution into the contributions table in the correct order
 		else:
 			raise InputError("User already contributed to this story.")
