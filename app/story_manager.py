ce833bec55129a916f6ffd42446b6016525a3cec
rengzhengcodes <rzhengcontacts@gmail.com>
2021-11-04 04:19:21 -0400

    optimizations made

--- app/story_manager.py
+++ app/story_manager.py
@@ -22,13 +22,9 @@ class Story_manager:
 		starter: starting prompt of the story'''
 		# adds a story to the list
 		if story not in self.get_catalog():
-			try: # if an error happens, it will catch it and say something is wrong with the story creation
-				self.c.execute("INSERT INTO stories (story) VALUES(?)", (story,)) #insert the new story into the catalog
-				self.c.execute("INSERT INTO contributions(contributor, story, addition, ordinal) VALUES(?, ?, ?, ?)", (creator, story, starter, 0)) # add the initial contribution to the contributions table
-				return True
-			except Exception as e: # if it does not it won't
-				print(e)
-				return False
+			self.c.execute("INSERT INTO stories (story) VALUES(?)", (story,)) #insert the new story into the catalog
+			self.c.execute("INSERT INTO contributions(contributor, story, addition, ordinal) VALUES(?, ?, ?, ?)", (creator, story, starter, 0)) # add the initial contribution to the contributions table
+			return True
 		else:
 			raise InputError("The name of this story is the same as another story.")
 		# TODO: We should make an exception for overlapping story titles and invalid titles
@@ -46,12 +42,10 @@ class Story_manager:
 		'''Gets the contributors to a particular story'''
 		self.c.execute("SELECT contributor FROM contributions WHERE story=?", (story,))
 		raw_roster = self.c.fetchall() #returns a tuple, where tuples inside contain the name
-		roster = list()
 		# takes names and puts it into 1 tuple instead of tuple in tuple
-		for row in raw_roster:
-			roster.append(row[0])
+		roster = tuple([row[0] for row in raw_roster])
 
-		return tuple(roster)
+		return roster
 
 	def insert_entry(self, usr:str, story:str, addition:str) -> bool:
 		'''Inserts an entry into the story. Notes user.'''
@@ -68,11 +62,8 @@ class Story_manager:
 		'''Returns a tuple of all the stories.'''
 		self.c.execute("SELECT * FROM stories")
 		catalog_tuple = tuple(self.c.fetchall())
-
-		catalog = list()
-		for tup in catalog_tuple: #removes story title from tuple so that it can be read more easily
-			catalog.append(tup[0])
-		catalog = tuple(catalog)
+		#removes story title from tuple so that it can be read more easily
+		catalog = tuple([tup[0] for tup in catalog_tuple])
 
 		return catalog
 
@@ -80,12 +71,10 @@ class Story_manager:
 		'''Returns a tuple of all the stories the user has contributed to.'''
 		self.c.execute("SELECT story FROM contributions WHERE contributor=?", (usr,))
 		raw_stories = self.c.fetchall()
-		stories = list()
 		#takes the story titles and puts them into 1 tuple instead of tuple in tuples
-		for row in raw_stories:
-			stories.append(row[0])
+		stories = tuple([row[0] for row in raw_stories])
 
-		return tuple(stories)
+		return stories
 
 	def get_story(self, story:str) -> str:
 		'''Returns the full text of a story'''
