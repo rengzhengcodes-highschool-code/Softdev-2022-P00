03ea2310c47269d6e03659c6368bf1a3f6508725
Lia-Nelson <jnelson20@stuy.edu>
2021-11-05 08:24:52 -0400

    Worked on routing for each method in stories.py (temporary file), organized methods

--- app/stories.py
+++ app/stories.py
@@ -3,28 +3,28 @@ import os
 import story_manager
 sm = Story_manager()
 
-if """appropritate interaction""":
-    new_story()
-
-if """appropritate interaction""":
-    new_contribution()
-
+@server.route('/story/new', methods=["POST"])
 def new_story():
     # need finished templates to insert parameters
     sm.create_story(self, creator:str, story:str, starter:str)
     # need to insert parameters, can use as paramters in render template
     sm.get_story(self, story:str)
     render_template(#for book)
+    return redirect(('/story'))
 
+@server.route('/story/add', methods=["POST"])
 def new_contribution():
     # need finished templates to insert parameters
     sm.insert_entry(self, usr:str, story:str, addition:str)
     render_template(# for book)
+    return redirect(('/story'))
 
+@server.route('/home', methods=["POST"])
 def render_home():
     # needs parameters, return tuple
     user_stories = sm.get_user_contributions(self, usr:str)
     render_template("templates/home.html", user, user_stories)
 
+@sever.route('/search', methods=["POST"])
 def render_search():
-    
+    render_template("templates/search.html")
